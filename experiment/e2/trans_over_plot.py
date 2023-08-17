fa24f8dd7e869346c69e5d5d2c290d8274433b27
Jurre Brandsen <jurre.brandsen@infosupport.com>
2023-08-17 15:58:11 +0200

    e2 new figures done

---
+++ experiment/e2/trans_over_plot.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_and_process_data(file_path, configuration, topic1, topic2):
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Time' column to datetime
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    # Remove throughput values of 0
+    df = df[df['Throughput'] > 0]
+
+    # Separate dataframe by topic
+    priority_df = df[df['Topic'] == topic1]
+    normal_df = df[df['Topic'] == topic2]
+
+    if topic1 == 'trans_topic_priority':
+        # also add the topic_priority to the priority_df
+        priority_df['Config_Topic'] = f'{configuration} Multi-hop Priority'
+    else:
+        # also add the topic_normal to the priority_df
+        priority_df['Config_Topic'] = f'{configuration} Priority'
+
+    if topic2 == 'trans_topic_normal':
+        # also add the topic_normal to the normal_df
+        normal_df['Config_Topic'] = f'{configuration} Multi-hop Normal'
+    else:
+        # also add the topic_normal to the normal_df
+        normal_df['Config_Topic'] = f'{configuration} Normal'
+
+    return priority_df, normal_df
+
+def plot_with_legend(all_data, color_dict, hatch_dict):
+    plt.figure(figsize=(6, 4))
+
+    ax = sns.boxplot(x="Throughput", y="Config_Topic", data=all_data, order=color_dict.keys(), palette=color_dict)
+
+    hatches = ["/", "o", "*", "\\"]
+    for hatch, patch in zip(hatches, ax.artists):
+        patch.set_hatch(hatch)
+
+    new_labels = ['Multi-hop' if 'Multi-hop Priority' in label.get_text() else 'Priority' if 'Priority' in label.get_text() else 'Multi-hop' if 'Multi-hop Normal' in label.get_text() else 'Normal' for label in ax.get_yticklabels()]
+    ax.set_yticklabels(new_labels)
+
+    # Create custom legend
+    legend_elements = [
+        plt.Line2D([0], [0], color=sns.color_palette()[3], lw=4, label='Star Topology'),
+        plt.Line2D([0], [0], color=sns.color_palette()[0], lw=4, label='Multi-hop Topology'),
+    ]
+
+    # place the legend outside the figure/axis
+    plt.legend(handles=legend_elements, loc='best')
+
+    plt.xlim(left=0)
+    plt.xlabel('Throughput (Dyconit Messages/s)')
+    plt.ylabel('Topic Type')
+    plt.tight_layout()
+    plt.savefig(f's-t_w2_p0-1_throughput_overhead_all.pdf', bbox_inches='tight', pad_inches=0.05, dpi=300)
+
+
+all_data = pd.DataFrame()
+
+# Load and process the data
+configurations = ['Star topology']
+configurations2 = ['Multi-hop topology']
+file_paths = [f'../star_topology/s_w2_p{i}_overhead_throughput.csv' for i in range(1, 2)]
+file_paths2 = [f'../trans_topology/t_w2_p{i}_overhead_throughput.csv' for i in range(1, 2)]
+file_paths3 = [f'../trans_topology/t_w2_p{i}_overhead_throughput.csv' for i in range(1, 2)]
+
+
+# ... [rest of the code remains unchanged]
+
+for config, path in zip(configurations, file_paths):
+    priority_df, normal_df = load_and_process_data(path, config, 'topic_priority', 'topic_normal')
+    all_data = pd.concat([all_data, priority_df, normal_df])
+
+    # Compute and print statistics for this configuration
+    for df, topic_type in [(priority_df, "Priority"), (normal_df, "Normal")]:
+        print(f"Configuration: {config} - {topic_type}")
+        print(f"Max Throughput: {df['Throughput'].max()}")
+        print(f"Mean Throughput: {df['Throughput'].mean()}")
+        print(f"25th Percentile: {df['Throughput'].quantile(0.25)}")
+        print(f"50th Percentile: {df['Throughput'].quantile(0.5)}")
+        print(f"75th Percentile: {df['Throughput'].quantile(0.75)}")
+        print("------------------------------------------------")
+
+for config, path in zip(configurations2, file_paths2):
+    priority_df, normal_df = load_and_process_data(path, config, 'topic_priority', 'topic_normal')
+    all_data = pd.concat([all_data, priority_df, normal_df])
+
+    # Compute and print statistics for this configuration
+    for df, topic_type in [(priority_df, "Priority"), (normal_df, "Normal")]:
+        print(f"Configuration: {config} - {topic_type}")
+        print(f"Max Throughput: {df['Throughput'].max()}")
+        print(f"Mean Throughput: {df['Throughput'].mean()}")
+        print(f"25th Percentile: {df['Throughput'].quantile(0.25)}")
+        print(f"50th Percentile: {df['Throughput'].quantile(0.5)}")
+        print(f"75th Percentile: {df['Throughput'].quantile(0.75)}")
+        print("------------------------------------------------")
+
+for config, path in zip(configurations2, file_paths3):
+    priority_df, normal_df = load_and_process_data(path, config, 'trans_topic_priority', 'trans_topic_normal')
+    all_data = pd.concat([all_data, priority_df, normal_df])
+
+    # Compute and print statistics for this configuration
+    for df, topic_type in [(priority_df, "Multi-hop Priority"), (normal_df, "Multi-hop Normal")]:
+        print(f"Configuration: {config} - {topic_type}")
+        print(f"Max Throughput: {df['Throughput'].max()}")
+        print(f"Mean Throughput: {df['Throughput'].mean()}")
+        print(f"25th Percentile: {df['Throughput'].quantile(0.25)}")
+        print(f"50th Percentile: {df['Throughput'].quantile(0.5)}")
+        print(f"75th Percentile: {df['Throughput'].quantile(0.75)}")
+        print("------------------------------------------------")
+
+# ... [rest of the code remains unchanged]
+# Color dictionary for different configurations
+color_dict = {
+    'Star topology Priority': sns.color_palette()[3],
+    'Multi-hop topology Priority': sns.color_palette()[0],
+    'Multi-hop topology Multi-hop Priority': sns.color_palette()[0],
+    'Star topology Normal': sns.color_palette()[3],
+    'Multi-hop topology Normal': sns.color_palette()[0],
+    'Multi-hop topology Multi-hop Normal': sns.color_palette()[0],
+}
+
+# Hatch dictionary for Priority/Normal distinction
+hatch_dict = {
+    'Priority': '*',
+    'Normal': '.'
+}
+
+sns.set_context("notebook")
+plot_with_legend(all_data, color_dict, hatch_dict)
+# Plot for all data
+# plt.figure(figsize=(6, 4))
+# ax = sns.boxplot(x="Throughput", y="Config_Topic", data=all_data, order=color_dict.keys(), palette=color_dict)
+
+# # Apply hatching based on Priority/Normal distinction
+# for i, box in enumerate(ax.artists):
+#     topic_type = 'Priority' if 'Priority' in color_dict.keys()[i] else 'Normal'
+#     box.set_hatch(hatch_dict[topic_type])
+
+# plt.xlim(left=0)
+# plt.xlabel('Throughput (messages/s)')
+# plt.ylabel('')
+
+# plt.tight_layout()
+# plt.savefig(f'e2/s-t_w2_p0-1_throughput_all.pdf', bbox_inches='tight', pad_inches=0.05, dpi=300)
---
+++ experiment/e3/overhead_plot.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_and_process_data(file_path, configuration):
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Time' column to datetime
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    # Remove throughput values of 0
+    df = df[df['Throughput'] > 0]
+
+    # Separate dataframe by topic
+    priority_df = df[df['Topic'] == 'topic_priority']
+    normal_df = df[df['Topic'] == 'topic_normal']
+
+    # Add a new 'Config_Topic' column to distinguish them
+    priority_df['Config_Topic'] = f'{configuration} Priority'
+    normal_df['Config_Topic'] = f'{configuration} Normal'
+
+    return priority_df, normal_df
+
+workloads = ['w3']
+
+for workload in workloads:
+    all_data = pd.DataFrame()
+    priority_data = pd.DataFrame()
+    normal_data = pd.DataFrame()
+
+    # Load and process the data
+    configurations = ['Undersaturated Workload', 'Fluctuating Workload', 'Oversaturated Workload']
+    file_paths = [f'star_topology/s_w{i}_p1_overhead_throughput.csv' for i in range(1, 4)]
+
+    for config, path in zip(configurations, file_paths):
+        priority_df, normal_df = load_and_process_data(path, config)
+
+        priority_data = pd.concat([priority_data, priority_df])
+        normal_data = pd.concat([normal_data, normal_df])
+
+    all_data = pd.concat([priority_data, normal_data])
+    # Create color dictionary
+    color_dict = {'Undersaturated Workload Priority': sns.color_palette()[3],  # orange
+                  'Fluctuating Workload Priority': sns.color_palette()[3],  # orange
+                  'Oversaturated Workload Priority': sns.color_palette()[3],  # blue
+                  'Undersaturated Workload Normal': sns.color_palette()[0],  # red
+                  'Fluctuating Workload Normal': sns.color_palette()[0],  # red
+                  'Oversaturated Workload Normal': sns.color_palette()[0],
+                  }  # green
+
+    # Plot for all data
+    plt.figure(figsize=(6, 4))
+    sns.boxplot(x="Throughput", y="Config_Topic", data=all_data, order=color_dict.keys(), palette=color_dict)
+    plt.xlim(left=0)
+    plt.xlabel('Throughput (Dyconit Messages/s)')
+    plt.ylabel('')
+
+    plt.tight_layout()
+    plt.savefig(f'e3/s_w1-3_p1_overhead_throughput_all.pdf', bbox_inches='tight', pad_inches=0.05, dpi=300)
